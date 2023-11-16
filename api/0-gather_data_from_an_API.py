040d4a493c17d60188571b2628affc44609b55dd
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-11-16 10:45:48 -0600

    Adding file 0 for API task

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This a basic get request from an API """
+
+from requests import get
+import json
+from sys import argv
+url = "https://jsonplaceholder.typicode.com/users/"
+url1 = "https://jsonplaceholder.typicode.com/todos/"
+
+if __name__ == "__main__":
+
+    response = get(url1, {"userId": argv[1]})  # , {"id": argv[1]})
+    if response.status_code == 200:
+        info = json.loads(response.text)
+    # print(info)
+    tasks = 0
+    completed = 0
+    completed_list = []
+    for item in info:
+        if item["completed"]:
+            completed += 1
+            completed_list.append(item["title"])
+        tasks += 1
+    response = get(url, {"id": argv[1]})
+    if response.status_code == 200:
+        info = json.loads(response.text)
+        name = info[0]["name"]
+    # print(name)
+    # print(f"{completed}/{tasks}")
+
+    print(f"Employee {name} is done with tasks({completed}/{tasks})")
+    for item in completed_list:
+        print(f"\t {item}")
